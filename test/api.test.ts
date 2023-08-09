4d7e2de5212d80cf9531283155198cacb89b887b
Kadek Darmayasa <77657401+kadekdarmayasa@users.noreply.github.com>
2023-08-09 10:15:22 +0800

    feat: finish the test of all api endpoint

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { app } from '../app';
 import { describe } from 'mocha';
+import { db } from '../lib/server.db';
 
 const expect = chai.expect;
 chai.use(chaiHttp);
@@ -19,6 +20,32 @@ describe('API ENDPOINT TESTING', () => {
         expect(res.body).to.have.property('portfolios');
         expect(res.body).to.have.property('testimonies');
         expect(res.body).to.have.property('blogs');
+        done();
+      });
+  });
+
+  it('GET: /api/v1/faqs', (done) => {
+    chai
+      .request(app)
+      .get('/api/v1/faqs')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('faqs');
+        expect(res.body.faqs).to.be.an('array');
+        done();
+      });
+  });
+
+  it('GET: /api/v1/blogs', (done) => {
+    chai
+      .request(app)
+      .get('/api/v1/blogs')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('blogs');
+        expect(res.body.blogs).to.be.an('array');
         done();
       });
   });
@@ -45,6 +72,151 @@ describe('API ENDPOINT TESTING', () => {
           expect(err).to.be.null;
           expect(res).to.have.status(200);
           done();
+        });
+    });
+  });
+
+  describe('GET: /api/v1/blog/:id', () => {
+    it('Should return 404 if could not be found', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/blog/121212')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(404);
+          expect(res.body).to.have.property('message');
+          expect(res.body.message).to.equal('Blog not found');
+          done();
+        });
+    });
+
+    it('Should return 200 if blog found', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/blog/1')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.blog).to.have.property('id');
+          done();
+        });
+    });
+  });
+
+  describe('POST: /api/v1/portfolios', () => {
+    it('Should return 200 if portfolios found', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/portfolios')
+        .send({ page: 1, serviceId: 1 })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.portfolios).to.be.an('array');
+          done();
+        });
+    });
+
+    it('Should return empty array when the page is out of range', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/portfolios')
+        .send({ page: 100, serviceId: 1 })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.portfolios).to.be.an('array');
+          expect(res.body.portfolios.length).to.equal(0);
+          done();
+        });
+    });
+
+    it("Should return empty array when the portfolio's serviceId is not match any service id", (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/portfolios')
+        .send({ page: 1, serviceId: 100 })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.portfolios).to.be.an('array');
+          expect(res.body.portfolios.length).to.equal(0);
+          done();
+        });
+    });
+
+    it('Should return empty array when service has no portfolio', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/portfolios')
+        .send({ page: 1, serviceId: 3 })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.portfolios).to.be.an('array');
+          expect(res.body.portfolios.length).to.equal(0);
+          done();
+        });
+    });
+  });
+
+  describe('GET: /api/v1/teams', () => {
+    it('Should return 200 if teams found', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/teams')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('teams');
+          expect(res.body.teams).to.be.an('array');
+          done();
+        });
+    });
+
+    it('Should not return the team with role of Admin', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/teams')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          const admin = res.body.teams.find((team: any) => team.role.name === 'Admin');
+          expect(admin).to.be.undefined;
+          done();
+        });
+    });
+  });
+
+  describe('POST: /api/v1/subscriber', () => {
+    it('Should return 400 if email is already subscribed', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/subscriber')
+        .send({ email: 'adiputrakadekdarmayasa@gmail.com' })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property('message');
+          expect(res.body.message).to.equal('Email already subscribed');
+          done();
+        });
+    });
+
+    it('Should return 200 if email is successfully subscribed', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/subscriber')
+        .send({ email: 'nengahsuastini@gmail.com' })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('message');
+          expect(res.body.message).to.equal('Subscribed successfully');
+
+          db.subscriber.findFirst({ orderBy: { id: 'desc' } }).then((subscriber) => {
+            db.subscriber.delete({ where: { id: subscriber?.id } }).then(() => done());
+          });
         });
     });
   });
